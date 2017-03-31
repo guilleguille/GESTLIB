e7dcb33a53bf46ba07d1ad7f61e5f4a3160f11cd
lebrel1 <salvadorgillopezrgmail.com>
2017-03-31 10:19:24 +0200

    Structs

---
+++ GestionBiblioteca/Structs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	
+	
+	char titulo[25], autor[30], editorial[20], clave[5];
+	int aPublicacion, nPaginas;
+	
+} tRegLibro;
+
+
+typedef struct{
+	
+	
+} tRegArticulo;
+
+
